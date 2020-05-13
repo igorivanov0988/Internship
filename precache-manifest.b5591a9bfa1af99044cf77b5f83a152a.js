f280da7e443334da9c6732271995425b1bf96e03
ANDREW <andrew@ANDREWs-MacBook-Pro.local>
2020-05-13 21:16:39 +0300

    Updates

---
+++ precache-manifest.b5591a9bfa1af99044cf77b5f83a152a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fb456322b423f56420010d8320ee25c",
+    "url": "/Internship/index.html"
+  },
+  {
+    "revision": "301132b3d423d900e1ab",
+    "url": "/Internship/static/css/2.54ae87a8.chunk.css"
+  },
+  {
+    "revision": "25020f7e0ce78583f332",
+    "url": "/Internship/static/css/main.335534e2.chunk.css"
+  },
+  {
+    "revision": "301132b3d423d900e1ab",
+    "url": "/Internship/static/js/2.565025e5.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/Internship/static/js/2.565025e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25020f7e0ce78583f332",
+    "url": "/Internship/static/js/main.0f2f73ab.chunk.js"
+  },
+  {
+    "revision": "d55f3335be0a069f60be",
+    "url": "/Internship/static/js/runtime-main.163b619d.js"
+  }
+]);
